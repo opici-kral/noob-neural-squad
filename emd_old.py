551cb00ff8843651cf754c18c26c6721b68d323a
Otomar "Seschly"Jeseter <34144652+opici-kral@users.noreply.github.com>
2018-05-28 17:51:09 +0200

    Update emd_old.py

--- emd_old.py
+++ emd_old.py
@@ -1,21 +1,24 @@
 import numpy as np
+from itertools import product
 
 q = [.125, .125, .125, .125, .125, .125, .125, .125]
 p = [0., 0., .16666667, .16666667, .16666667, .16666667, .16666667, .16666667]
 # p = [[[0.,0.16666667], [0.16666667, 0.16666667]],[[0.16666667, 0.16666667],[0.16666667,0.16666667]]]
 # q = [[[.125, .125],[.125,.125]],[[.125,.125],[.125,.125]]]
 
+
 def emd(p,q):
     emd = 0
     td = 0
-    for i in range(0,len(p)-1):
+    for i in range(0, len(p)-1):
         emd = p[i] + emd - q[i]
         td += abs(emd)
     return td
 
-def kullback_leibner(p,q):
+
+def kullback_leibner(p, q):
     DKL = 0
-    for i in range(0,len(p)-1):
+    for i in range(0, len(p)-1):
         if p[i] == 0:
             DKL += 0
         else:
@@ -23,9 +26,62 @@ def kullback_leibner(p,q):
     return DKL
 
 
-dist_emd = emd(p,q)
-dist_dkl = kullback_leibner(p,q)
+def variations_generator(x, x_len):
+    sample = list()
+    for i in product(x, repeat=x_len):
+        sample.append(i)
+    return sample
 
 
-print "EMD(p,q) = ", dist_emd
-print "Kullback_Leibner(p,q) = ", dist_dkl
+def and_gate(list):
+    result = []
+    for i in (0, len(list)-1):
+        a = list[i][0]
+        b = list[i][1]
+        print(list[i])
+        print("a:",a)
+        print("b:",b)
+        if a == 1 and b == 1:
+            result.append(1)
+        else:
+            result.append(0)
+
+    return result
+
+
+def or_gate(a, b):
+    result = []
+    if a == 0 and b == 0:
+        result.append(0)
+    else:
+        result.append(1)
+    return result
+
+
+def xor_gate(a, b):
+    result = []
+    if (a == 1 and b == 1) or (a == 0 and b == 0):
+        result.append(0)
+    else:
+        result.append(1)
+    return result
+
+bin_set = [0, 1]
+
+my_3_space = variations_generator(bin_set, 3)
+my_2_space = variations_generator(bin_set, 2)
+
+dist_emd = emd(p, q)
+dist_dkl = kullback_leibner(p, q)
+
+
+print("")
+print("EMD(p,q) = ", dist_emd)
+print("")
+print("Kullback_Leibner(p,q) = ", dist_dkl)
+print("")
+print(my_3_space)
+print("")
+print(my_2_space)
+print("")
+print(and_gate(my_2_space))
